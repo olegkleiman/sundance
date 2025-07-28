240b7f36e2bc902ec810e7ac322f564739610bcf
Oleg Kleiman <oleg_kleyman@hotmail.com>
2025-07-28 20:11:57 +0300

    OpenAI for Genkit

--- packages/server/src/genkit.ts
+++ packages/server/src/genkit.ts
@@ -2,14 +2,16 @@ import { genkit } from 'genkit/beta';
 import { googleAI, gemini15Flash } from '@genkit-ai/googleai';
 import { vertexAI } from '@genkit-ai/vertexai';
 import { logger } from 'genkit/logging';
+import openAI, { gpt4oMini } from 'genkitx-openai';
 
 export const ai = genkit({
     plugins: [
         googleAI(), // automatically look for GOOGLE_API_KEY in your environment.
         vertexAI(),
+        openAI({ apiKey: process.env.OPENAI_API_KEY })
     ],
     promptDir: './llm_prompts',
-    model: gemini15Flash
+    model: gpt4oMini, // or gemini15Flash
     // enableTracingAndMetrics: true
 })
 
